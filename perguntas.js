cc9b0042774af8f660bd6d58c4c42b6a74c9e7f0
erik4maia <145574046+erik4maia@users.noreply.github.com>
2024-10-25 08:57:34 -0300

    25/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+    'História',
+    'Qual foi a principal consequência da  Segunda Guerra mundial?',
+    'A criação da Organização das Nações Unidas (ONU) e a divisão do mundo em blocos (Ocidental e Oriental). '
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+    'Curiosidade',
+    'Qual o aplicativo mais usado no mundo?',
+    'Instagram.'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+    'Futebol',
+    'Quantos clubes disputam a Copa Libertadores anualmente?',
+    '47 clubes.'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+    'Curiosidade',
+    'Quantas fases a lua possui e quais são elas?',
+    'Possui quatro fases,sendo elas: nova, crescente, cheia e minguante.'
 )
